8fa5e68c1ef29c6f790e95a6dd578b008e164e95
Jake Archibald <jakearchibald@theteam.co.uk>
2011-04-08 17:17:36 +0100

    Basic working example

--- design-overlay.js
+++ design-overlay.js
@@ -1,5 +1,6 @@
 (function(window, document, undefined) {
-	// check that everything we need has loaded. Otherwise load it in
+	// check that everything we need has loaded. Otherwise load it in.
+	// init() is called once everything is ready
 	(function() {
 		var head = document.getElementsByTagName('head')[0],
 			thisBasePath,
@@ -50,7 +51,7 @@
 			interval = setInterval(function() {
 				if ( isReady() ) {
 					clearInterval(interval);
-					init();
+					$(init);
 				}
 				else if (window.jQuery && !loadingJqui) {
 					loadingJqui = true;
@@ -61,7 +62,48 @@
 		}
 	})();
 	
+	function loadImg(url, callback) {
+		$('<img/>').load(function() {
+			callback(this);
+		}).attr('src', url);
+	}
+	
 	function init() {
-		console.log('init')
+		var $ = jQuery,
+			$controls,
+			$overlay = $('<div class="design-overlay"/>').appendTo(document.body).draggable(),
+			$slider;
+			
+		$controls = $(
+			'<div class="design-overlay-controls">' +
+				'<div><label for="design-overlay-url">Image URL</label> <input id="design-overlay-url" type="url" class="design-overlay-text" /></div>' +
+				'<div><label for="design-overlay-opacity">Opacity</label> <div id="design-overlay-slider"></div></div>' +
+				'<div><input id="design-overlay-toggle" type="checkbox" class="checkbox" checked /> <label for="design-overlay-toggle">Enable</label></div>' +
+			'</div>' +
+		'').appendTo(document.body);
+		
+		$('#design-overlay-url').change(function() {
+			loadImg(this.value, function(img) {
+				$overlay.css({
+					'background-image': 'url(' + img.src + ')',
+					top: 0,
+					left: ( $(window).width() - img.width ) / 2,
+					width: img.width,
+					height: img.height
+				});
+			});
+		});
+		
+		$slider = $('#design-overlay-slider').slider({
+			min: 0,
+			max: 1,
+			step: 0.01
+		}).bind('slide', function(event, ui) {
+			$overlay.css( 'opacity', ui.value );
+		});
+		
+		$('#design-overlay-toggle').change(function() {
+			$overlay[this.checked ? 'show' : 'hide']();
+		});
 	}
 })(window, document, undefined);
